fe5714b3754e84de93d872c038a6373a323197e1
jothi <esakkijothika358@gmail.com>
2025-04-20 19:46:10 +0530

    final changes

--- server/index.js
+++ server/index.js
@@ -7,20 +7,6 @@ const busrouter = require("./module/bus.module");
 
 let app = express();
 
-//  Enable preflight requests and CORS for credentials
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://bus-buddy-73fm.vercel.app");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
-
-//  Handle preflight OPTIONS request for all routes
-app.options('*', cors({
-  origin: "https://bus-buddy-73fm.vercel.app",
-  credentials: true
-}));
 
 //  JSON parsing and CORS setup
 app.use(express.json());
