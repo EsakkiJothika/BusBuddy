2a603d8874e8ecd9822858c6eaef27af9af7766f
jothi <esakkijothika358@gmail.com>
2025-04-20 19:43:25 +0530

    final changes

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ let app = express();
 
 //  Enable preflight requests and CORS for credentials
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://busbooking-eta.vercel.app");
+  res.header("Access-Control-Allow-Origin", "https://bus-buddy-73fm.vercel.app");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
@@ -18,14 +18,14 @@ app.use((req, res, next) => {
 
 //  Handle preflight OPTIONS request for all routes
 app.options('*', cors({
-  origin: "https://busbooking-eta.vercel.app",
+  origin: "https://bus-buddy-73fm.vercel.app",
   credentials: true
 }));
 
 //  JSON parsing and CORS setup
 app.use(express.json());
 app.use(cors({
-  origin: "https://busbooking-eta.vercel.app",
+  origin: "https://bus-buddy-73fm.vercel.app",
   credentials: true
 }));
 app.use(cookieparser());
